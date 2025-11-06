ab536a3febf66bf544445189143bc16620c31204
nichuanfang <f18326186224@gmail.com>
2025-11-06 10:36:10 +0800

    fix: 修复音乐模块tg交互bug

--- internal/bot/dispatch/music.go
+++ internal/bot/dispatch/music.go
@@ -22,7 +22,7 @@ func (s *Session) HandleMusic(p music.Processor) error {
 	// 下载阶段
 	utils.InfoWithFormat("[Telegram] 下载中...")
 	err := p.DownloadMusic(s.Link, func(progress string) {
-		bot.Edit(msg, fmt.Sprintf("✅ 已识别【**%s**】链接\n\n🎵 %s", p.Name(), progress), tb.ModeMarkdown)
+		_, _ = bot.Edit(msg, fmt.Sprintf("✅ 已识别【**%s**】链接\n\n🎵 %s", p.Name(), progress), tb.ModeMarkdown)
 	})
 	if err != nil {
 		utils.ErrorWithFormat("[Telegram] 下载失败: %v", err)
@@ -41,7 +41,7 @@ func (s *Session) HandleMusic(p music.Processor) error {
 
 	// 入库
 	utils.InfoWithFormat("[Telegram] 整理成功，开始入库...")
-	_, _ = bot.Edit(msg, fmt.Sprintf("✅ 已识别 **%s** 链接\n\n🎵 开始入库...", p.Name()), tb.ModeMarkdown)
+	_, _ = bot.Edit(msg, fmt.Sprintf("✅ 已识别【**%s**】链接\n\n🎵 开始入库...", p.Name()), tb.ModeMarkdown)
 	if err := p.TidyMusic(); err != nil {
 		utils.ErrorWithFormat("[Telegram] 文件入库失败: %v", err)
 		_, _ = bot.Edit(msg, fmt.Sprintf("⚠️ 文件入库失败：\n```\n%s\n```", utils.TruncateString(err.Error(), 400)), tb.ModeMarkdown)
