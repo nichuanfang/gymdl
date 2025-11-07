90665339da4ce90e3f3c633215a2d069924fe47c
nichuanfang <f18326186224@gmail.com>
2025-11-08 03:05:49 +0800

    fix: yt-dlp下载指定输出目录

--- processor/music/youtube_music.go
+++ processor/music/youtube_music.go
@@ -85,8 +85,9 @@ func (p *YoutubeMusicProcessor) DownloadCommand(url string) *exec.Cmd {
 		"--no-playlist",         //严格列表模式
 		"--audio-format", "aac", //格式为aac
 		"--audio-quality", "0", //最高质量
-		"--embed-metadata",  //添加基本元数据 除封面外 还缺失 `专辑` `专辑艺术家` `歌词` 需配合mtw手动刮削
-		"--embed-thumbnail", //嵌入封面
+		"--embed-metadata",                                  //添加基本元数据 除封面外 还缺失 `专辑` `专辑艺术家` `歌词` 需配合mtw手动刮削
+		"--embed-thumbnail",                                 //嵌入封面
+		"-o", filepath.Join(p.tempDir, "%(title)s.%(ext)s"), // 输出路径
 		url,
 	}
 	return exec.Command("yt-dlp", args...)
