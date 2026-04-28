8446afadd27e72624c0b11aaf630b158c1013c3a
nichuanfang <f18326186224@gmail.com>
2026-04-28 16:14:35 +0800

    feat: 新增BiliBili音乐下载器

--- processor/music/bilibili_music.go
+++ processor/music/bilibili_music.go
@@ -18,72 +18,45 @@ import (
 
 /* ---------------------- 结构体与构造方法 ---------------------- */
 
-type YoutubeMusicProcessor struct {
+type BilibiliMusicProcessor struct {
 	cfg     *config.Config
 	tempDir string
 	songs   []*SongInfo
 }
 
-type Config struct {
-	AdditionalConfig struct {
-		EnableYoutubeCookie bool
-	}
-	CookieCloud struct {
-		CookieFilePath string
-		CookieFile     string
-	}
-}
-
-// Format JSON 解析结构
-type ytFormat struct {
-	FormatID string `json:"format_id"`
-	Ext      string `json:"ext"`
-}
-
-type ytInfo struct {
-	Formats []ytFormat `json:"formats"`
-}
-
 // Init  初始化
-func (p *YoutubeMusicProcessor) Init(cfg *config.Config) {
+func (p *BilibiliMusicProcessor) Init(cfg *config.Config) {
 	p.cfg = cfg
 	p.songs = make([]*SongInfo, 0)
 	p.tempDir = processor.BuildOutputDir(YoutubeTempDir)
 }
 
-// AudioFormat 结构用来解析 yt-dlp -j 输出
-type AudioFormat struct {
-	FormatID string `json:"format_id"`
-	Ext      string `json:"ext"`
-	Acodec   string `json:"acodec"`
-}
-
 /* ---------------------- 基础接口实现 ---------------------- */
 
-func (p *YoutubeMusicProcessor) Name() processor.LinkType {
-	return processor.LinkYoutubeMusic
+func (p *BilibiliMusicProcessor) Name() processor.LinkType {
+	return processor.LinkBiliBiliMusic
 }
 
-func (p *YoutubeMusicProcessor) Songs() []*SongInfo {
+func (p *BilibiliMusicProcessor) Songs() []*SongInfo {
 	return p.songs
 }
 
 /* ------------------------ 下载逻辑 ------------------------ */
-func (p *YoutubeMusicProcessor) DownloadMusic(url string, callback func(string)) error {
+func (p *BilibiliMusicProcessor) DownloadMusic(url string, callback func(string)) error {
 	start := time.Now()
 
-	utils.InfoWithFormat("[YoutubeMusic] 🎵 开始下载: %s", url)
+	utils.InfoWithFormat("[BiliBiliMusic] 🎵 开始下载: %s", url)
 
 	cmd := p.DownloadCommand(url)
 	callback("命令构建完成，开始下载...")
 	if cmd == nil {
 		return errors.New("download command build failed")
 	}
-	utils.DebugWithFormat("[YoutubeMusic] 执行命令: %s", strings.Join(cmd.Args, " "))
+	utils.DebugWithFormat("[BiliBiliMusic] 执行命令: %s", strings.Join(cmd.Args, " "))
 
 	// 创建临时目录
 	if err := processor.CreateOutputDir(p.tempDir); err != nil {
-		utils.ErrorWithFormat("[YoutubeMusic] ❌ 创建临时目录失败: %v", err)
+		utils.ErrorWithFormat("[BiliBiliMusic] ❌ 创建临时目录失败: %v", err)
 		return err
 	}
 
@@ -92,22 +65,22 @@ func (p *YoutubeMusicProcessor) DownloadMusic(url string, callback func(string))
 	logOut := strings.TrimSpace(string(output))
 	if err != nil {
 		_ = processor.RemoveTempDir(p.tempDir)
-		utils.ErrorWithFormat("[YoutubeMusic] ❌ 下载失败: %v\n输出:\n%s", err, logOut)
+		utils.ErrorWithFormat("[BiliBiliMusic] ❌ 下载失败: %v\n输出:\n%s", err, logOut)
 		return fmt.Errorf("yt-dlp 下载失败: %w", err)
 	}
 
 	// 输出调试信息，仅当有日志内容时
 	if logOut != "" {
-		utils.DebugWithFormat("[YoutubeMusic] 下载输出:\n%s", logOut)
+		utils.DebugWithFormat("[BiliBiliMusic] 下载输出:\n%s", logOut)
 	}
 
-	utils.InfoWithFormat("[YoutubeMusic] ✅ 下载完成（耗时 %v）", time.Since(start).Truncate(time.Millisecond))
+	utils.InfoWithFormat("[BiliBiliMusic] ✅ 下载完成（耗时 %v）", time.Since(start).Truncate(time.Millisecond))
 	callback(fmt.Sprintf("下载完成（耗时 %v）", time.Since(start).Truncate(time.Millisecond)))
 	return nil
 }
 
 // getAvailableFormats 获取可用格式（优化版：流式解析 + 映射表）
-func (p *YoutubeMusicProcessor) getAvailableFormats(url string, cookiePath string) (map[string]bool, error) {
+func (p *BilibiliMusicProcessor) getAvailableFormats(url string, cookiePath string) (map[string]bool, error) {
 	args := []string{
 		"--no-playlist",
 		"--skip-download",
@@ -116,7 +89,7 @@ func (p *YoutubeMusicProcessor) getAvailableFormats(url string, cookiePath strin
 		"--no-progress",
 	}
 
-    args = append(args, "--cookies", cookiePath, "--extractor-args", "youtube:player_client=web_music")
+    args = append(args, "--cookies", cookiePath)
 
 	args = append(args, "-F", url)
 
@@ -136,10 +109,10 @@ func (p *YoutubeMusicProcessor) getAvailableFormats(url string, cookiePath strin
 
 	// 目标格式映射表
 	targetExt := map[string]string{
-		"141": "m4a",
-		"774": "webm",
-		"251": "webm",
-		"140": "m4a",
+		"30280": "m4a",
+        "30250": "m4a",
+        "30232": "m4a",
+		"30216": "m4a",
 	}
 
 	scanner := bufio.NewScanner(stdoutPipe)
@@ -164,13 +137,13 @@ func (p *YoutubeMusicProcessor) getAvailableFormats(url string, cookiePath strin
 }
 
 // 生成下载命令
-func (p *YoutubeMusicProcessor) DownloadCommand(url string) *exec.Cmd {
+func (p *BilibiliMusicProcessor) DownloadCommand(url string) *exec.Cmd {
 	cookiePath := filepath.Join(p.cfg.CookieCloud.CookieFilePath, p.cfg.CookieCloud.CookieFile)
 	start := time.Now()
 	// 获取可用格式
 
 	formats, err := p.getAvailableFormats(url, cookiePath)
-	utils.InfoWithFormat("[YoutubeMusic] ✅ 成功解析链接（耗时 %v）", time.Since(start).Truncate(time.Millisecond))
+	utils.InfoWithFormat("[BiliBiliMusic] ✅ 成功解析链接（耗时 %v）", time.Since(start).Truncate(time.Millisecond))
 	if err != nil {
 		// 这里可按你的 utils 警告逻辑
 		return nil
@@ -180,22 +153,14 @@ func (p *YoutubeMusicProcessor) DownloadCommand(url string) *exec.Cmd {
 	var formatID string
 	var postArgs []string
 	switch {
-	case formats["141"]:
-		formatID = "141" // 高质量 AAC
-	case formats["774"]:
-		formatID = "774" // 高质量 opus
-		postArgs = []string{
-			"--audio-format", "aac",
-			"--postprocessor-args", "-c:a libfdk_aac -vbr 5 -afterburner 1",
-		}
-	case formats["251"]:
-		formatID = "251" // 中等质量 opus
-		postArgs = []string{
-			"--audio-format", "aac",
-			"--postprocessor-args", "-c:a libfdk_aac -vbr 5 -afterburner 1",
-		}
-	case formats["140"]:
-		formatID = "140" // 中等质量 AAC
+	case formats["30280"]:
+		formatID = "30280" // 高码率
+    case formats["30250"]:
+        formatID = "30250" // 192k 码率
+    case formats["30232"]:
+        formatID = "30232" // 128k 码率
+    case formats["30216"]:
+        formatID = "30216" // 64k 码率
 	default:
 		formatID = "bestaudio" // 默认用最佳音质 转aac
 		postArgs = []string{
@@ -217,6 +182,8 @@ func (p *YoutubeMusicProcessor) DownloadCommand(url string) *exec.Cmd {
 
     args = append(args, "--cookies", cookiePath, "--extractor-args", "youtube:player_client=web_music")
 	args = append(args, "-o", filepath.Join(p.tempDir, "%(title)s.%(ext)s"))
+    // B 站非常看重 Referer
+    args = append(args, "--add-header", "Referer:https://www.bilibili.com")
 	args = append(args, postArgs...)
 	args = append([]string{"-f", formatID}, args...)
 	args = append(args, url)
@@ -224,7 +191,7 @@ func (p *YoutubeMusicProcessor) DownloadCommand(url string) *exec.Cmd {
 	return exec.Command("yt-dlp", args...)
 }
 
-func (p *YoutubeMusicProcessor) BeforeTidy() error {
+func (p *BilibiliMusicProcessor) BeforeTidy() error {
 	songs, err := ReadMusicDir(p.tempDir, processor.DetermineTidyType(p.cfg), p)
 	if err != nil {
 		return err
@@ -234,21 +201,21 @@ func (p *YoutubeMusicProcessor) BeforeTidy() error {
 	return nil
 }
 
-func (p *YoutubeMusicProcessor) NeedRemoveDRM() bool {
+func (p *BilibiliMusicProcessor) NeedRemoveDRM() bool {
 	return false
 }
 
-func (p *YoutubeMusicProcessor) DRMRemove() error {
+func (p *BilibiliMusicProcessor) DRMRemove() error {
 	return nil
 }
 
-func (p *YoutubeMusicProcessor) TidyMusic() error {
+func (p *BilibiliMusicProcessor) TidyMusic() error {
 	files, err := os.ReadDir(p.tempDir)
 	if err != nil {
 		return fmt.Errorf("读取临时目录失败: %w", err)
 	}
 	if len(files) == 0 {
-		utils.WarnWithFormat("[YoutubeMusic] ⚠️ 未找到待整理的音乐文件")
+		utils.WarnWithFormat("[BiliBiliMusic] ⚠️ 未找到待整理的音乐文件")
 		return errors.New("未找到待整理的音乐文件")
 	}
 
@@ -262,17 +229,17 @@ func (p *YoutubeMusicProcessor) TidyMusic() error {
 	}
 }
 
-func (p *YoutubeMusicProcessor) EncryptedExts() []string {
+func (p *BilibiliMusicProcessor) EncryptedExts() []string {
 	return make([]string, 0)
 }
 
-func (p *YoutubeMusicProcessor) DecryptedExts() []string {
+func (p *BilibiliMusicProcessor) DecryptedExts() []string {
 	return []string{".aac", ".m4a", ".flac", ".mp3", ".ogg"}
 }
 
 /* ------------------------ 拓展方法 ------------------------ */
 // 整理到本地
-func (p *YoutubeMusicProcessor) tidyToLocal(files []os.DirEntry) error {
+func (p *BilibiliMusicProcessor) tidyToLocal(files []os.DirEntry) error {
 	dstDir := p.cfg.Tidy.DistDir
 	if dstDir == "" {
 		_ = processor.RemoveTempDir(p.tempDir)
@@ -285,7 +252,7 @@ func (p *YoutubeMusicProcessor) tidyToLocal(files []os.DirEntry) error {
 
 	for _, f := range files {
 		if !utils.FilterMusicFile(f, p.EncryptedExts(), p.DecryptedExts()) {
-			utils.DebugWithFormat("[YoutubeMusic] 跳过非音乐文件: %s", f.Name())
+			utils.DebugWithFormat("[BiliBiliMusic] 跳过非音乐文件: %s", f.Name())
 			continue
 		}
 		src := filepath.Join(p.tempDir, f.Name())
@@ -294,7 +261,7 @@ func (p *YoutubeMusicProcessor) tidyToLocal(files []os.DirEntry) error {
 		if err != nil {
 			return err
 		}
-		utils.InfoWithFormat("[YoutubeMusic] 📦 已整理: %s", dst)
+		utils.InfoWithFormat("[BiliBiliMusic] 📦 已整理: %s", dst)
 	}
 	// 清除临时目录
 	err := processor.RemoveTempDir(p.tempDir)
@@ -305,7 +272,7 @@ func (p *YoutubeMusicProcessor) tidyToLocal(files []os.DirEntry) error {
 }
 
 // 整理到webdav
-func (p *YoutubeMusicProcessor) tidyToWebDAV(files []os.DirEntry, webdav *core.WebDAV) error {
+func (p *BilibiliMusicProcessor) tidyToWebDAV(files []os.DirEntry, webdav *core.WebDAV) error {
 	if webdav == nil {
 		_ = processor.RemoveTempDir(p.tempDir)
 		return errors.New("WebDAV 未初始化")
@@ -313,16 +280,16 @@ func (p *YoutubeMusicProcessor) tidyToWebDAV(files []os.DirEntry, webdav *core.W
 
 	for _, f := range files {
 		if !utils.FilterMusicFile(f, p.EncryptedExts(), p.DecryptedExts()) {
-			utils.DebugWithFormat("[YoutubeMusic] 跳过非音乐文件: %s", f.Name())
+			utils.DebugWithFormat("[BiliBiliMusic] 跳过非音乐文件: %s", f.Name())
 			continue
 		}
 
 		filePath := filepath.Join(p.tempDir, f.Name())
 		if err := webdav.Upload(filePath); err != nil {
-			utils.WarnWithFormat("[YoutubeMusic] ☁️ 上传失败 %s: %v", f.Name(), err)
+			utils.WarnWithFormat("[BiliBiliMusic] ☁️ 上传失败 %s: %v", f.Name(), err)
 			continue
 		}
-		utils.InfoWithFormat("[YoutubeMusic] ☁️ 已上传: %s", f.Name())
+		utils.InfoWithFormat("[BiliBiliMusic] ☁️ 已上传: %s", f.Name())
 	}
 	// 清除临时目录
 	err := processor.RemoveTempDir(p.tempDir)
